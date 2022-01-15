dd05194f7fb99d8cce491cc0cbd68252d6b9e345
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-01-15 15:42:06 +0530

    added route

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express=require('express');
 const app=express();
+const bodyParser=require('body-parser');
+const configuration=require('./config');
+const router=re
+
+const PORT=configuration.port;
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//starting the server at port defined in config.js
+app.listen(PORT,()=>{
+    console.log(`server started at: http://localhost:${PORT}`);
+});
